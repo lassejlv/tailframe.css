5228f05224b2e5b2528f2315d5f011a46b1fdbca
lassev05 <vestergaardlasse2@gmail.com>
2023-10-13 11:07:36 +0200

    Homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.jsx"],
+  content: ["./src/**/*.html"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
